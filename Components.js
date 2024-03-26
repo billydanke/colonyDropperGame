76b718a4608f8c18b2ae2b39449a20581457b0e3
billydanke <johnjo490@gmail.com>
2024-03-26 15:08:37 -0500

    refactor start

---
+++ Components.js
@@ -0,0 +1,81 @@
+function Star(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 2;
+    this.height = 2;
+    this.color = '#f5da42';
+  
+    this.move = function(amt) {
+      this.y += amt;
+  
+      if(this.y > c.height) {
+        this.y = -this.height;
+        this.x = Math.floor(Math.random() * c.width);
+      }
+    }
+  
+    this.draw = function() {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+  }
+  
+  function Sprite(x,y,img,scale,isDrawn=true) {
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+    this.width = img.width;
+    this.height = img.height;
+    this.img = img;
+    this.isDrawn = isDrawn;
+  
+    this.draw = function() {
+      if(isDrawn) {
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(this.img,this.x-(this.width/2),this.y-(this.height/2), this.width*this.scale, this.height*this.scale);
+      }
+    }
+  }
+  
+  function Planet(x,y,img,scale,isDrawn=false) {
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+    this.width = 16;
+    this.height = 16;
+    this.img = img;
+    this.isDrawn = isDrawn;
+    this.inTransition = false;
+    this.inOutTransition = false;
+    this.description = "";
+    this.surfaceScan = "";
+    this.moistureScan = "";
+    this.atmosphereScan = "";
+    this.probeResult = "";
+    this.landingSuccess = "";
+    this.landingFailure = "";
+  
+    this.draw = function() {
+      if(this.isDrawn) {
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(this.img,this.x,this.y, this.width*this.scale, this.height*this.scale);
+      }
+    }
+    this.transition = function(stopLocation) {
+      if(this.y < stopLocation) {
+        this.y += 2;
+      } else {
+        this.inTransition = false;
+        if(hasArrived) {
+          hasArrived = false;
+        } else {
+          hasArrived = true;
+        }
+      }
+    }
+  
+    this.reset = function() {
+      this.isDrawn = false;
+      this.y = -100;
+    }
+  }
---
+++ textStorage.js
@@ -0,0 +1,85 @@
+
+descList = ["This cold, small dwarf planet sits on the outskirts of a solar system of 12 planets.",
+            "This water world rests gracefully in the Goldilocks zone around its Red Giant star.",
+            "This tropical water planet sits 3rd from its star, basking in its warm shine.",
+            "This water world sits in the 4th position from its star, seemingly experiencing a range of warm temperatures while showing 2.3x Earth gravity.",
+            "This planet rests within the Goldilocks zone of its white dwarf star, and appears to be extremely similar to Earth in composition.",
+            "This planet sits in the 2nd position away from its star, with flowing rivers, vast oceans, and large landmasses.",
+            "It appears that either the long-range sensors misanalyzed this planet, or it was very recently habitable and since destroyed by an astronomical catastrophe.",
+            "This gleaming yellow planet is the 12th and final planet in its solar system, however, its massive blue star makes this planet extremely hot."
+        ];
+
+pScanList = ["Its surface is covered in thick sheets of solid ice, with large, ore-rich ridges running along the planet.",
+            "Surface scans indicate an abundance of uranium deposits just beneath the frozen south pole of the planet.",
+            "Surface scans detect a moderate level of resources beneath the ocean floor, with rich resource deposits on the poles.",
+            "Its surface is primarily covered in oceans, with small resource-weak islands scattered throughout.",
+            "Surface scans indicate a wide range of geographic features and Earth-like terrain.",
+            "Surface scans indicate water, soil, and rich ore deposits on and beneath the surface, very similar in nature to Earth, except for a series of large craters and radioactive elements.",
+            "Surface scans detect a 3-inch layer of Chromium completely covering the surface, with a massive tear down the equator reaching its upper mantle.",
+            "Surface scans show that this planet is covered in massive sprawling sand dunes."
+        ];
+
+moistureList = ["Moisture has not been detected on the surface.",
+                "Moisture readings show a humidity gradient similar to Earth's.",
+                "Moisture readings indicate very strong humidity levels.",
+                "The humidity varies, however it is generally considered high here.",
+                "Moisture readings indicate a varying level of humidity throughout the planet.",
+                "Moisture readings indicate a very similar humidity gradient to Earth.",
+                "Moisture readings indicate a low level of humidity in the air.",
+                "Moisture readings show that there is no humidity and almost no water present on the planet."
+            ];
+
+atmosList = ["The planet's atmosphere is extremely thin, what little gas present being unbreathable to humans.",
+            "The atmosphere is slightly thicker than Earth and is composed primarily of Carbon Dioxide and noble gasses.",
+            "The atmospheric composition is reasonably habitable for humans.",
+            "The atmospheric composition is perfect for humans to breathe, with an astonishing 43% oxygen content.",
+            "Atmospheric readings show a nominal atmospheric pressure, with a composition of 31% Oxygen and 12% Carbon Dioxide.",
+            "Atmospheric Analysis shows an Earthlike composition, with a high count of radioactive particles in the air.",
+            "Atmospheric readings show a strong magnetosphere and breathable air, however high levels of ash are also present.",
+            "Atmospheric Analysis shows that the air is partially breathable, but could be harmful to breathe for long periods of time."
+        ];
+
+probeResultsList = ["While the surface has yielded few results, probing deep beneath the surface has resulted in the discovery of liquid water and a mantle layer.",
+                    "Sending the probe out resulted in the discovery of multiple microbial life forms frozen in the south pole of the planet.",
+                    "Probe scans have resulted in the discovery of a wide range of aquatic life, some of which are potentially hazardous. While scanning the water for signs of microbial life, a large leech-like aquatic creature latched on to the probe. The probe stopped sending readings soon after.",
+                    "Probing the surface has revealed multiple tribal civilizations living on the islands here. Confused and scared by the probe's space-age technology, they destroy the probe as it analyzes sand samples.",
+                    "Initial probe readings indicate a vast variety of plant and animal life, along with a late atomic-age civilization that dominates the planet. Fascinated by the probe's arrival, they attempt first contact, however their language is currently unknown.",
+                    "Probe readings indicate that this planet appears to have once been the home to a relatively advanced species, however, due to the many craters and radioactive materials along the surface, it appears that they have wiped themselves out by nuclear annihilation some decades in the past.",
+                    "As the probe shoots across the surface, it begins to detect irregularities under the chrome layer. They appear almost like statues, rising smoothly from the ground, almost as if whatever creatures lie beneath were covered over instantly. Reaching the planet's tear, the probe's feed cuts off abruptly.",
+                    "Probe scans reveal a plant species with a strange spice substance that is believed to have hallucinogenic properties, along with a species of gargantuan worm-like creatures that have adapted to survive in the planet's extreme conditions. Behavioral analysis of these creatures shows extreme hostility."
+                ];
+
+landingSuccList = ["The ship lands gently on the frozen world, using minimal thrust to combat the extremely low gravity here. After deploying the Colony Deployment Unit and waking the colonists, a thriving colony is established, albeit with weak bones, but made wealthy from the rich resources here.",
+                "The ship touches down gently on the south pole and extends its Colony Deployment Unit. With protective suits, the colonists begin their new future, slowly terraforming their new world and finding inoculations to the bacteria frozen solid below their feet.",
+                "The colony ship AI performs controlled firings of its atmospheric thrusters, safely bringing the ship and its colonists to a somewhat shallow region of water. Releasing the Colony Deployment Unit proved difficult, however with the help of the colonists, it is eventually fully operational. The colonists here spend their days fighting off aquatic life as they mine resources to construct their submerged cities and ships.",
+                "The ship burns hard and uses aerobraking maneuvers to eventually perform a strategic landing on the largest island in a cluster. The Colony Deployment Unit is deployed and the colonists are awoken. Due to the intense gravitational pull, constant attack by freakishly large insects, and the lack of resources, the colony eventually devolves into tribalism along with their native neighbors.",
+                "The ship performs a stable burn as it performs its landing near a native metropolis. As the Colony Deployment Unit is released, a group of native diplomats approaches the freshly awakened colonists. With the time the AI has had to analyze the species native language, it serves as an interpreter for negotiations. In the end, the colonists and the native population live in harmony with one another, blending into one people.",
+                "The ship burns to slow down against the atmosphere, landing in the middle of a large crater. With the aid of lead-lined suits, the colonists assemble the Colony Deployment Unit, spending the years to come cleaning up radiation and rediscovering this past civilization's history.",
+                "The AI experiences a series of glitches as it seemingly feels called to the planet. It begins its landing routine, slowly descending toward this mysterious metal world. Upon landing, the AI feels an unknowing urge to eject the cryo-pods onto the surface. Upon doing so, both the ship and the pods are covered instantly in the shiny metal, all now connected to the great hivemind controlling this world.",
+                "The ship lands near the base of a dune, where there seems to be an abundance of spice plants. The colonists use heat-resistant suits and rebreathers, however over time they build a tolerance to the heat and air. Some colonists are lost to the worm creatures, but they soon learn to repel their attacks. They learn to find and grow spice plants to successfully sustain a growing population, and use the plants' effects to further their space travels."
+            ];
+
+landingFailList = ["The ship experiences an electrical glitch causing the engines to fire full-forward toward the dwarf planet. The impact is fatal. There are no survivors.",
+                "The ship lands, but is forced to use the full force of its titanic engines. The heat begins to melt the ice below the massive landing legs of the ship. Even before the colonists are able to settle into their new habitation unit, they begin to feel sick. Within 4 days all of the colonists are dead.",
+                "A glitch in Cosmo's landing positioning system resulted in a splashdown target much deeper than what was expected. The water pressure crushes the outer hull of the vessel, leaving its frozen passengers deep within the murky darkness to rest permanently.",
+                "Upon its top speed of reentry, the primary heat shield is ripped off, causing a flameout in the ship and cryo-pods. Maybe one day the native peoples inhabiting this world will tell their children about the strange star shooting across the sky, simmering into nothing far before it ever reached the surface.",
+                "The ship performs a stable burn as it performs its landing near a native metropolis. As the Colony Deployment Unit is released, a buildup of military forces surrounds the ship. After a pause, they begin to fire on the ship. Programmed to preserve the colonist's lives, the AI uses its defense systems to vaporize the native forces. In fear of total annihilation, the natives use their nuclear arsenal against the colony ship. All colonists are lost.",
+                "The ship ignites its deceleration engines, however an error in the ship's accelerometer causes it to shut down its engines prematurely, resulting in the ship impacting the surface at a rapid speed. In the end, nothing remained of the ship but just another radioactive crater on the planet's scarred surface.",
+                "The AI experiences a series of glitches as it seemingly feels called to the planet. It begins its landing routine, aggressively firing its engines in a feverish attempt to reach the surface of this mysterious metal world. Realizing too late that the AI's decision-making was impaired, it was unable to slow down before impacting the surface. In its final effort to save the colonists, it ejects the cryo-pods, instantly being covered in the metal upon landing. No one survived.",
+                "The ship's hull and core systems are heavily damaged from the extreme heat entering the planet's atmosphere. When the ship lands, the people are immediately attacked by a worm creature. With damaged defense systems, the AI is helpless to defend the colonists. The colonists and ship are swallowed whole, doomed to die inside the belly of a space-worm."
+            ];
+
+planetImgList = [];
+// Populate the planet source list
+for(let i = 0;i<8;i++) {
+    planetImgList.push(new Image());
+  }
+  
+  planetImgList[0].src = 'gfx/planets/planetIce.png';
+  planetImgList[1].src = 'gfx/planets/planetWaterTemperate.png';
+  planetImgList[2].src = 'gfx/planets/planetWaterTropical.png';
+  planetImgList[3].src = 'gfx/planets/planetIslandsTropical.png';
+  planetImgList[4].src = 'gfx/planets/planetEarthlike.png';
+  planetImgList[5].src = 'gfx/planets/planetPostnuclear.png';
+  planetImgList[6].src = 'gfx/planets/planetAstronomicalAnomoly.png';
+  planetImgList[7].src = 'gfx/planets/planetDesert.png';
